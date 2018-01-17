9f9acbb9a3013a384161ed4d15bb83171da31f1c
Jas502n <jas502n@gmail.com>
2018-01-17 15:02:51 +0800

    Weather

---
+++ app/src/main/java/com/shaoyu/www/coolweather_shaoyu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shaoyu.www.coolweather_shaoyu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
